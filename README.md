81f083d43d4c18f203d21834e6359180ae9633bb
iturbideM <martiniturbidenoria@gmail.com>
2026-03-07 02:31:23 -0300

    basic ray tracer

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ray-Tracer
+
+Basic ray tracer implemented in C++. Following the tutorial [Ray Tracing in One Weekend](https://raytracing.github.io/).
---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "camera.h"
+#include "rtweekend.h"
+
+#include "hittable_list.h"
+#include "sphere.h"
+#include "vec3.h"
+
+int main() {
+    // World
+    hittable_list world;
+
+    world.add(make_shared<sphere>(point3(1, 0, -3), 0.5));
+    world.add(make_shared<sphere>(point3(-1, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+
+    camera cam;
+
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth = 50;
+
+    cam.render(world);
+}
